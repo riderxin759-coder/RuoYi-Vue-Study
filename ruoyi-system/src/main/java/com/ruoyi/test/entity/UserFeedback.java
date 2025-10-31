3b7bc49f08097e2e6dfc3943f2b2318d9e971654
Your Name <email@example.com>
2025-10-31 11:35:03 +0800

    修复了实体类没有getter和setter

--- ruoyi-system/src/main/java/com/ruoyi/test/entity/UserFeedback.java
+++ ruoyi-system/src/main/java/com/ruoyi/test/entity/UserFeedback.java
@@ -6,4 +6,43 @@ public class UserFeedback {
     private String question;
     private String feedback;
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public String getFeedback() {
+        return feedback;
+    }
+
+    public void setFeedback(String feedback) {
+        this.feedback = feedback;
+    }
 }
